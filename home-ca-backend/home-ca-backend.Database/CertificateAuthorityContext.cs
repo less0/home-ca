72468fa59761b7af3d0dba79bf694aaf1fb0058f
Paul Kertscher <paul.kertscher+code@gmail.com>
2024-03-09 14:03:54 +0100

    Added database project with EF Core support to store data

---
+++ home-ca-backend/home-ca-backend.Database/CertificateAuthorityContext.cs
@@ -0,0 +1,42 @@
+﻿using home_ca_backend.Core.CertificateAuthorityServerAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace home_ca_backend.Database;
+
+public class CertificateAuthorityContext : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public CertificateAuthorityContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public CertificateAuthorityContext()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("(Default)"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CertificateAuthority>()
+            .HasKey(ca => ca.Id);
+        modelBuilder.Entity<CertificateAuthority>()
+            .Property(ca => ca.Id)
+            .HasConversion(
+                certificateAuthorityId => certificateAuthorityId.Guid,
+                guid => new(guid));
+        modelBuilder.Entity<Leaf>()
+            .HasKey(leaf => leaf.Id);
+        modelBuilder.Entity<Leaf>()
+            .Property(leaf => leaf.Id)
+            .HasConversion(
+                leafId => leafId.Guid,
+                guid => new(guid));
+    }
+}
---
+++ home-ca-backend/home-ca-backend.Database/Migrations/20240309130203_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace home_ca_backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CertificateAuthority",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PemCertificate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EncryptedCertificate = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PemPrivateKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CertificateAuthorityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CertificateAuthority", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CertificateAuthority_CertificateAuthority_CertificateAuthorityId",
+                        column: x => x.CertificateAuthorityId,
+                        principalTable: "CertificateAuthority",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Leaf",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EncryptedCertificate = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    PemCertificate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PemPrivateKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CertificateAuthorityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leaf", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Leaf_CertificateAuthority_CertificateAuthorityId",
+                        column: x => x.CertificateAuthorityId,
+                        principalTable: "CertificateAuthority",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CertificateAuthority_CertificateAuthorityId",
+                table: "CertificateAuthority",
+                column: "CertificateAuthorityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leaf_CertificateAuthorityId",
+                table: "Leaf",
+                column: "CertificateAuthorityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leaf");
+
+            migrationBuilder.DropTable(
+                name: "CertificateAuthority");
+        }
+    }
+}
